b0d0ad03b8bf44384482dea64f225efc5ea8f37d
victor-kiptoo-001 <veelangat680@gmail.com>
2025-03-22 18:28:32 +0300

    Added constants

---
+++ utils/constants.ts
@@ -0,0 +1,28 @@
+export const links = [
+    {name: 'Home', url: '/'},
+    {name: 'Car Parts', url: '/parts'},
+    {name: 'About', url: '/about'},
+    {name: 'Services', url: '/services'},
+    {name: 'Contact Us', url: '/contact'}
+]
+
+export const baseUrl = 'https://subesh.onrender.com/api'
+// export const baseUrl = 'http://localhost:1337/api'
+
+export const carImages = [
+    'https://images.pexels.com/photos/3807120/pexels-photo-3807120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/190574/pexels-photo-190574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/4116196/pexels-photo-4116196.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
+    'https://images.pexels.com/photos/3807277/pexels-photo-3807277.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/3807811/pexels-photo-3807811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    'https://images.pexels.com/photos/4116203/pexels-photo-4116203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    'https://images.pexels.com/photos/8478223/pexels-photo-8478223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    'https://images.pexels.com/photos/8478223/pexels-photo-8478223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+]
+
+export const storeInfo = {
+    name: 'Subesh Auto Services',
+    phone: '083 209 2668',
+    location: 'Hamberg, Roodepoort',
+    fullLocation: 'Eeufees Avenue, Hamberg Roodepoort'
+}
