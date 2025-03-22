3fed915f49c28e679c06130e665a8f48432f075a
victor-kiptoo-001 <veelangat680@gmail.com>
2025-03-22 22:43:35 +0300

    deployment issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5546d6253f79849a"; }
+/******/ 		__webpack_require__.h = function() { return "a9a4450661c841f2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
